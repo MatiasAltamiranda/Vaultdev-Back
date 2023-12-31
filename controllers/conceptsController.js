cfe4866207e1f68f0c60cd969c1e8ccc45fba5ae
Matias <matias_lma94@hotmail.com>
2023-12-31 10:02:54 -0300

    Added search function by id and by query in controller

--- controllers/conceptsController.js
+++ controllers/conceptsController.js
@@ -23,3 +23,26 @@ exports.createConcepts = async (req,res) =>{
         res.status(500).json({message : "Hubo un error al crear el concepto"})
     }
 }
+
+exports.getConceptsById = async (req, res) => {
+    try {
+        const {id} = req.params
+        const concept = await Concept.findById(id)
+        return res.status(200).json({ok: true, concept : concept});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Hubo un error al obtener el concepto id" });
+    }
+};
+
+exports.getConceptsByParams = async (req, res) => {
+    try {
+        const title = req.query.q;
+        const regex = new RegExp(title, 'i');
+        const concepts = await Concept.find({ title: regex });
+        return res.status(200).json({ ok: true, concepts: concepts });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Hubo un error al obtener los conceptos" });
+    }
+};
--- routes/conceptsRoute.js
+++ routes/conceptsRoute.js
@@ -1,11 +1,18 @@
 const Router = require("express");
-const { getConcepts, createConcepts } = require("../controllers/conceptsController");
+const { getConcepts, createConcepts, getConceptsById,getConceptsByParams} = require("../controllers/conceptsController");
 
 const router = Router();
 
 router.route("/")
 .get(getConcepts)
 .post(createConcepts)
+
+
+router.route("/search")
+.get(getConceptsByParams)
+
+router.route("/:id")
+ .get(getConceptsById) 
 
 
 
