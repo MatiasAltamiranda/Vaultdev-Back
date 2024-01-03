fb419bb77c2acb2ceba57945b0162724eb14467c
Matias <matias_lma94@hotmail.com>
2024-01-02 22:16:23 -0300

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const connectDB = require('./config/db');
 const express = require("express");
+const cors = require("cors")
 
 // Routes
 const conceptsRoute = require("./routes/conceptsRoute")
 
 connectDB();
 const app = express();
-app.use(express.json({limit : "110kb"})),
-app.use("/api/v1/concepts" , conceptsRoute)
+app.use(cors());
+app.use(express.json({limit : "110kb"}));
+app.use("/api/v1/concepts" , conceptsRoute);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Corriendo en el puerto ${process.env.PORT}`)
